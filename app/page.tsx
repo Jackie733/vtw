20bcd5d8ead1eb1d92fd91b7cdb741589e83d84d
Jackie Tsien <jackie10673311@gmail.com>
2024-07-17 10:59:59 +0800

    style: add title and style

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-6xl font-bold text-transparent">
+        View the world
+      </h1>
+    </main>
   );
 }
