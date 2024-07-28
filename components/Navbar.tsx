16ed2ce471e1eadc917584022d2372544eaad625
Jackie Tsien <jackie10673311@gmail.com>
2024-07-28 19:59:58 +0800

    chore: vtw app header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,8 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <header className="dark:bg-stone-90 fixed top-0 z-50 mx-auto w-full bg-white px-4 shadow sm:px-20 dark:border-b dark:border-stone-600">
-      View the world
+    <header className="dark:bg-stone-90 fixed top-0 z-50 mx-auto flex w-full items-center px-4 shadow dark:border-b dark:border-stone-600 sm:px-20">
+      <h1 className="flex-1">View the world</h1>
       <ModeToggle />
     </header>
   );
